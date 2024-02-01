1df06a431683063d1caa76252dc0b29d5e4a6dc6
Hasan Tunçay <hasantuncay2635@gmail.com>
2024-02-01 07:34:51 +0300

    while

--- Programming Basics/Repetition with while/Exercise 2/src/Task.kt
+++ Programming Basics/Repetition with while/Exercise 2/src/Task.kt
@@ -2,7 +2,13 @@
 package repetitionWithWhileExercise2
 
 fun sum(number: Int): Int {
-  TODO()
+  var sum = 0
+  var i = 0
+   while ( i <= number){
+    sum += i
+     i++
+  }
+  return sum
 }
 
 fun main() {
--- Programming Basics/Repetition with while/Exercise 3/src/Task.kt
+++ Programming Basics/Repetition with while/Exercise 3/src/Task.kt
@@ -2,9 +2,25 @@
 package repetitionWithWhileExercise3
 
 fun sumOfEven(n: Int): Int {
-  TODO()
+ var sum = 0
+   var i = 0
+  while (i <= n && i % 2 == 0){
+    sum += i
+    i+=2
+  }
+  return sum
 }
-
+//fun sumOfEven(n: Int): Int {
+//  var result = 0
+//  var i = 0
+//  while (i <= n) {
+//    if (i % 2 == 0) {
+//      result += i
+//    }
+//    i++
+//  }
+//  return result
+//}
 fun main() {
   println(sumOfEven(10))  // 30
 }
