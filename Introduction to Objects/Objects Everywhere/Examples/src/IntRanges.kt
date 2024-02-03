8c5bcfe32f746fc6edf30effb56b58aa5b69687f
Hasan Tunçay <hasantuncay2635@gmail.com>
2024-02-04 00:00:44 +0300

    statements and expressions

--- Introduction to Objects/Objects Everywhere/Examples/src/IntRanges.kt
+++ Introduction to Objects/Objects Everywhere/Examples/src/IntRanges.kt
@@ -1,8 +1,9 @@
 // ObjectsEverywhere/IntRanges.kt
 
 fun main() {
-  val r1 = IntRange(0, 10)
+  val r1 = IntRange(0, 11)
   val r2 = IntRange(5, 7)
+  val a= 5
   println(r1)
   println(r2)
 }
--- Programming Basics/Summary 1/Exercise 2/src/Task.kt
+++ Programming Basics/Summary 1/Exercise 2/src/Task.kt
@@ -2,11 +2,17 @@
 package summaryIExercise2
 
 fun other(s: String): String {
-  TODO()
+    var str = ""
+    for (i in 0..s.length - 1) {
+        if (i % 2 == 0) {
+            str += s[i]
+        }
+    }
+    return str
 }
 
 fun main() {
-  println(other("cement"))
+    println(other("cement"))
 }
 /* Output:
 cmn
